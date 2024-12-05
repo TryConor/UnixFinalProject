c5d87b52f7c6400b2a00f0bfb47e4e6216718ffd
samschottler <samschottler@gmail.com>
2024-12-05 15:40:29 -0600

    Update icon.sh

--- icon.sh
+++ icon.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
+# Script: icon.sh
+# Purpose: Generate a .desktop file with user-selected script and icon using Zenity.
 
-# Use Zenity to prompt user to select the script (.sh file) to run and store in a variable
+# Prompt the user to select a script file
+script_file=$(zenity --file-selection --title="Select a Script File" --file-filter="*.sh" --text="Choose a script to associate with the desktop icon.")
+if [ -z "$script_file" ]; then
+    zenity --error --text="No script selected. Exiting."
+    exit 1
+fi
 
+# Prompt the user to select an icon image
+icon_file=$(zenity --file-selection --title="Select an Icon Image" --file-filter="*.png *.jpg *.svg" --text="Choose an icon for the desktop shortcut.")
+if [ -z "$icon_file" ]; then
+    zenity --error --text="No icon selected. Exiting."
+    exit 1
+fi
 
-# If no script is selected, exit
+# Prompt the user to enter a name for the desktop shortcut
+shortcut_name=$(zenity --entry --title="Shortcut Name" --text="Enter a name for the desktop shortcut:")
+if [ -z "$shortcut_name" ]; then
+    zenity --error --text="No shortcut name provided. Exiting."
+    exit 1
+fi
 
-
-# Use Zenity to prompt user to select an image to use as the icon and store in a variable
-
-
-# If no image is selected, exit
-
-
-
-# Use Zenity to prompt user to enter a name for the desktop entry and store in a variable
-
-
-# If no name is entered, use a default name
-
-
-# Define the path for the .desktop file (in the current directory) and store in a variable
-
-
-# Create the .desktop file using echo commands
-# You can echo the content with the variables that you created
-# using all the variables that were stored for path
-# and zenity. The first line will be redirected >
-# the following lines will be added with >>
-
-
-# Copy the .desktop file to the user's desktop
-
+# Generate the .desktop file content
+desktop_file_path="$HOME/Desktop/$shortcut_name.desktop"
+cat << EOF > "$desktop_file_path"
+[Desktop Entry]
+Type=Application
+Name=$shortcut_name
+Exec=$script_file
+Icon=$icon_file
+Terminal=true
+EOF
 
 # Make the .desktop file executable
+chmod +x "$desktop_file_path"
 
+# Notify the user of success
+zenity --info --text="Desktop shortcut created successfully: $desktop_file_path"
 
-# Use Zenity to notify user that the .desktop file has been created and moved
